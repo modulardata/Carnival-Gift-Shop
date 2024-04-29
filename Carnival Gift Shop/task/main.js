6af788091511822982ab1e6cfb2d096862c2aaf4
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-04-30 00:44:12 +0200

    Stage 1/5

---
+++ Carnival Gift Shop/task/main.js
@@ -0,0 +1,21 @@
+let welcomeText = [
+    "WELCOME TO THE CARNIVAL GIFT SHOP!",
+    "Hello friend! Thank you for visiting the carnival!",
+    "Here's the list of gifts:",
+    "",
+    "Teddy Bear",
+    "Big Red Ball",
+    "Huge Bear",
+    "Candy",
+    "Stuffed Tiger",
+    "Stuffed Dragon",
+    "Skateboard",
+    "Toy Car",
+    "Basketball",
+    "Scary Mask"];
+
+
+let sentence;
+for ( sentence of welcomeText ) {
+    console.log(sentence);
+}
---
+++ Carnival Gift Shop/task/tests.py
@@ -0,0 +1,5 @@
+from test.tests import CarnivalGiftShopTest
+
+
+if __name__ == '__main__':
+    CarnivalGiftShopTest('main').run_tests()
