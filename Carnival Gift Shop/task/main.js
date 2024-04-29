ab1ddf7c4295cadbbb405112ffce3c89094c5282
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-04-30 00:46:07 +0200

    Stage 3/5

--- Carnival Gift Shop/task/main.js
+++ Carnival Gift Shop/task/main.js
@@ -1,34 +1,77 @@
-const welcomeText = [
-    "WELCOME TO THE CARNIVAL GIFT SHOP!",
-    "Hello friend! Thank you for visiting the carnival!",
-    "Here's the list of gifts:"
-];
+const input = require('sync-input');
+const data = require('./gift_shop_data.json');
 
-const gifts = [
-    { name: "Teddy Bear", price: 10, id: 1 },
-    { name: "Big Red Ball", price: 5, id: 2 },
-    { name: "Huge Bear", price: 50, id: 3 },
-    { name: "Candy", price: 8, id: 4 },
-    { name: "Stuffed Tiger", price: 15, id: 5 },
-    { name: "Stuffed Dragon", price: 30, id: 6 },
-    { name: "Skateboard", price: 100, id: 7 },
-    { name: "Toy Car", price: 25, id: 8 },
-    { name: "Basketball", price: 20, id: 9 },
-    { name: "Scary Mask", price: 75, id: 10 }
-];
 
-function logWelcomeText() {
-    welcomeText.forEach((sentence, index) => {
-        console.log(`${sentence}`);
-    });
-}
+const giftShop = {
+    welcomeMessage: function () {
+        for (let i = 0; i < data['welcome_message'].length; i++) {
+            console.log(data['welcome_message'][i]);
+        }
+        console.log('\r');
+    },
+    showGiftsList: function () {
+        for (let i = 0; i < data['gifts_list'].length; i++) {
+            console.log(
+                `${data['gifts_list'][i].id}- ${data['gifts_list'][i].name}, Cost: ${data['gifts_list'][i].price} tickets`
+            );
+        }
+        console.log('\r');
+    },
+    chose: function () {
+        console.log('What do you want to do?');
+        let question = input(
+            '1-Buy a gift 2-Add tickets 3-Check tickets 4-Show gifts\n'
+        );
+        switch (question) {
+            case '1':
+                giftShop.buy();
+                break;
+            case '2':
+                giftShop.addTickets();
+                break;
+            case '3':
+                giftShop.checkTickets(data['tickets']);
+                break;
+            case '4':
+                giftShop.showGifts();
+                break;
+            default:
+                console.log('Unknown command!');
+        } // switch
+    },
+    buy: function () {
+        let question = input('Enter the number of the gift you want to get:');
+        for (let i = 0; i < data['gifts_list'].length; i++) {
 
-function logGifts() {
-    gifts.forEach((gift, index) => {
-        console.log(`${index + 1}- ${gift.name}, Cost: ${gift.price} tickets`);
-    });
-}
+            if (parseInt(question) === parseInt(data['gifts_list'][i].id)) {
+                let total = data['tickets'] - data['gifts_list'][i].price;
+                console.log(
+                    `Here you go, one ${data['gifts_list'][i].name}!`
+                );
+                console.log(`Total tickets: ${total + '\n'}Have a nice day!`);
+                break;
+            }
+        }
+    },
+    addTickets: function () {
+        let question = input('Enter the ticket amount: ');
+        data['tickets'] += parseInt(question);
+        console.log(`Total tickets: ${data['tickets'] + '\n'}Have a nice day!`);
+    },
+    checkTickets: function (amount) {
+        console.log(`Total tickets: ${amount + '\n'}Have a nice day!`);
+    },
+    showGifts: function () {
+        console.log('Here\'s the list of gifts:' + '\n');
+        giftShop.showGiftsList();
+        console.log('Have a nice day!');
+    },
+};
 
-logWelcomeText();
-console.log('\r');
-logGifts();
+const __main__ = () => {
+    giftShop.welcomeMessage();
+    giftShop.showGiftsList();
+    giftShop.chose();
+};
+
+__main__();
