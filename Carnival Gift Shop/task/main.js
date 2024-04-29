932ba21a86afdc7690facf3d36de4f5c9da191ee
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-04-30 00:45:03 +0200

    Stage 2/5

--- Carnival Gift Shop/task/main.js
+++ Carnival Gift Shop/task/main.js
@@ -1,21 +1,34 @@
-let welcomeText = [
+const welcomeText = [
     "WELCOME TO THE CARNIVAL GIFT SHOP!",
     "Hello friend! Thank you for visiting the carnival!",
-    "Here's the list of gifts:",
-    "",
-    "Teddy Bear",
-    "Big Red Ball",
-    "Huge Bear",
-    "Candy",
-    "Stuffed Tiger",
-    "Stuffed Dragon",
-    "Skateboard",
-    "Toy Car",
-    "Basketball",
-    "Scary Mask"];
+    "Here's the list of gifts:"
+];
 
+const gifts = [
+    { name: "Teddy Bear", price: 10, id: 1 },
+    { name: "Big Red Ball", price: 5, id: 2 },
+    { name: "Huge Bear", price: 50, id: 3 },
+    { name: "Candy", price: 8, id: 4 },
+    { name: "Stuffed Tiger", price: 15, id: 5 },
+    { name: "Stuffed Dragon", price: 30, id: 6 },
+    { name: "Skateboard", price: 100, id: 7 },
+    { name: "Toy Car", price: 25, id: 8 },
+    { name: "Basketball", price: 20, id: 9 },
+    { name: "Scary Mask", price: 75, id: 10 }
+];
 
-let sentence;
-for ( sentence of welcomeText ) {
-    console.log(sentence);
+function logWelcomeText() {
+    welcomeText.forEach((sentence, index) => {
+        console.log(`${sentence}`);
+    });
 }
+
+function logGifts() {
+    gifts.forEach((gift, index) => {
+        console.log(`${index + 1}- ${gift.name}, Cost: ${gift.price} tickets`);
+    });
+}
+
+logWelcomeText();
+console.log('\r');
+logGifts();
