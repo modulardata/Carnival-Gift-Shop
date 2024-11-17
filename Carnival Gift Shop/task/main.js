13ad11e5031889f4198b519336fa9e22e294927d
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-11-17 12:48:27 +0100

    Stage 5/5

--- Carnival Gift Shop/task/main.js
+++ Carnival Gift Shop/task/main.js
@@ -1,7 +1,6 @@
 const input = require('sync-input');
 const data = require('./gift_shop_data.json');
 
-
 const giftShop = {
     welcomeMessage: function () {
         for (let i = 0; i < data['welcome_message'].length; i++) {
@@ -15,7 +14,7 @@ const giftShop = {
                 `${data['gifts_list'][i].id}- ${data['gifts_list'][i].name}, Cost: ${data['gifts_list'][i].price} tickets`
             );
         }
-        console.log('\r');
+        console.log();
     },
     chose: function () {
         console.log('What do you want to do?');
@@ -33,41 +32,75 @@ const giftShop = {
                 giftShop.checkTickets(data['tickets']);
                 break;
             case '4':
-                giftShop.showGifts();
+                giftShop.showGiftsList();
                 break;
             case '5':
                 giftShop.out();
                 break;
             default:
-                console.log('Unknown command!');
+                console.log('Please enter a valid number!\n');
+                giftShop.chose();
         } // switch
     },
     buy: function () {
-        let question = input('Enter the number of the gift you want to get:');
-        for (let i = 0; i < data['gifts_list'].length; i++) {
-            if (parseInt(question) === parseInt(data['gifts_list'][i].id)) {
-                let total = data['tickets'] - data['gifts_list'][i].price;
-                console.log(
-                    `Here you go, one ${data['gifts_list'][i].name}!`
-                );
-                console.log(`Total tickets: ${total + '\n'}Have a nice day!`);
-                data['gifts_list'].splice(i, 1);
-                break;
-            }
+        if (data['gifts_list'].length === 0) {
+            console.log("Wow! There are no gifts to buy.");
+            console.log();
+            giftShop.chose();
+            return;
         }
+
+        const question = input('Enter the number of the gift you want to get: ');
+        if (isNaN(question) || question.trim() === '') {
+            console.log('Please enter a valid number!');
+            console.log();
+            giftShop.chose();
+            return;
+        }
+
+        const giftNumber = parseInt(question);
+        const gift = data['gifts_list'].find(g => g.id === giftNumber);
+
+        if (!gift) {
+            console.log('There is no gift with that number!');
+            console.log();
+            giftShop.chose();
+            return;
+        }
+
+        if (data['tickets'] >= gift.price) {
+            let total = data['tickets'] - gift.price;
+            console.log(`Here you go, one ${gift.name}!`);
+            console.log(`Total tickets: ${total}`);
+            data['tickets'] = total;
+            data['gifts_list'] = data['gifts_list'].filter(g => g.id !== gift.id);
+        } else {
+            console.log("You don't have enough tickets to buy this gift.");
+        }
+
+        console.log(`Total tickets: ${data['tickets']}`);
+        console.log();
+        giftShop.chose();
     },
     addTickets: function () {
         let question = input('Enter the ticket amount: ');
-        data['tickets'] += parseInt(question);
-        console.log(`Total tickets: ${data['tickets'] + '\n'}Have a nice day!` + '\n');
+        if (isNaN(question) || question.trim() === '' || parseInt(question) < 0 || parseInt(question) > 1000) {
+            console.log('Please enter a valid number between 0 and 1000.');
+            console.log();
+            giftShop.chose();
+            return;
+        }
+
+        let amount = parseInt(question);
+        data['tickets'] += amount;
+        console.log(`Total tickets: ${data['tickets']}`);
+        console.log();
+        giftShop.chose();
     },
     checkTickets: function (amount) {
-        console.log(`Total tickets: ${amount + '\n'}Have a nice day!`);
-    },
-    showGifts: function () {
-        console.log('Here\'s the list of gifts:' + '\n');
-        giftShop.showGiftsList();
-        console.log('Have a nice day!');
+        console.log(`Total tickets: ${amount}`);
+        console.log();
+        giftShop.chose();
     },
     out: function () {
         console.log('Have a nice day!');
@@ -76,12 +109,11 @@ const giftShop = {
 };
 
 const __main__ = () => {
-do {
-
+    do {
         giftShop.welcomeMessage();
         giftShop.showGiftsList();
         giftShop.chose();
-} while (true);
+    } while (true);
 };
 
 __main__();
